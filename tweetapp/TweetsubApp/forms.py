d2387f266665bf4639a6a41d97d5137f83562d30
saurabhbhatt <bhattsourav4@gmail.com>
2025-04-08 23:49:03 +0530

    add django user registration feature

--- tweetapp/TweetsubApp/forms.py
+++ tweetapp/TweetsubApp/forms.py
@@ -1,10 +1,18 @@
 from django import forms
 from .models import Tweet
-
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 
 class TweetForm(forms.ModelForm):
     class Meta:
         model = Tweet
-        fields = ['text', 'photo']
+        fields = ['text', 'photo']     # here fields are array because we created tweetform
 
+
+
+class UserRegistrationForm(UserCreationForm):
+    email = forms.EmailField()
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')  # here fields are tuple because we are using django created registration form
--- tweetapp/TweetsubApp/views.py
+++ tweetapp/TweetsubApp/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-from .forms import TweetForm
+from .forms import TweetForm, UserRegistrationForm
 from .models import Tweet
-from django.shortcuts import get_object_or_404, redirect   #ORM to interact DB
+from django.shortcuts import get_object_or_404, redirect   #ORM to interact with DB
+from django.contrib.auth.decorators import login_required  #decorators are  used for tasks like authentication, authorization, or caching
+from django.contrib.auth import login
 
 # Create your views here.
 
@@ -13,6 +15,8 @@ def tweet_list(request):
     tweets = Tweet.objects.all().order_by('-created_at')
     return render(request, 'tweet_list.html', {'tweets':tweets} )
 
+
+@login_required             #(Login authentication) it makes sure that user is loged in before going to create a tweet 
 def tweet_create(request):
     if request.method == "POST":
         form = TweetForm(request.POST, request.FILES)
@@ -26,6 +30,7 @@ def tweet_create(request):
     return render(request, 'tweet_form.html', {'form' : form})
     
 
+@login_required
 def tweet_edit(request, tweet_id):
     tweet = get_object_or_404(Tweet, pk = tweet_id , user = request.user)
     if request.method == 'POST':
@@ -40,7 +45,7 @@ def tweet_edit(request, tweet_id):
     return render(request, 'tweet_form.html', {'form' : form})
     
 
-
+@login_required
 def tweet_delete(request, tweet_id):
     tweet = get_object_or_404(Tweet, pk=tweet_id, user = request.user)
     if request.method == "POST":
@@ -49,3 +54,17 @@ def tweet_delete(request, tweet_id):
 
     return render(request, 'tweet_confirm_delete.html', {'tweet' : tweet})
     
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)    # not save form in DB
+            user.set_password(form.cleaned_data(['password1']))
+            user.save()        # commit = false ni h toh form DB m save hoga
+            login(request, user)  #automatically logged in user
+            return redirect('tweet_list')
+    else:
+        form = UserRegistrationForm()
+        return render(request, 'registration/register.html', {'form' : form})
